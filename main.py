c8dd0ada03a9dabd9a4141026632864a3af57ee1
raftouch <rafanelkin@gmail.com>
2025-05-06 20:06:09 +0200

    add main script

---
+++ main.py
@@ -0,0 +1,20 @@
+from transformers import pipeline
+from datasets import load_dataset
+import soundfile as sf
+import torch
+import os
+
+os.makedirs('output', exist_ok=True)
+
+device = 0 if torch.cuda.is_available() else -1
+
+synthesiser = pipeline("text-to-speech", "microsoft/speecht5_tts", device=device)
+
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+
+speech = synthesiser("What's your favorite coffee?", forward_params={"speaker_embeddings": speaker_embedding})
+
+sf.write("data/favorite_coffee.wav", speech["audio"], samplerate=speech["sampling_rate"])
+
+print("Done!")
